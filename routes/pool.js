56e37eb4f418102df554f6c51ddbb7cf91ffe65d
BhumiShrivastava <165683191+BhumiShrivastava@users.noreply.github.com>
2025-09-20 12:56:31 +0530

    Adding Backend Project

--- routes/pool.js
+++ routes/pool.js
@@ -1,13 +1,9 @@
-const mysql = require('mysql2');
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
-module.exports = pool.promise();
+var mysql=require('mysql')
+var pool=mysql.createConnection({
+ host:'localhost',
+ user:'root',
+ password:'1234',
+ database:'moviedb',
+ multipleStatements:true,
+})
+module.exports = pool;
